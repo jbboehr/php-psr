eda4687c97f0b6cafb52af989c144c162a42094c
John Boehr <jbboehr@gmail.com>
2015-12-15 13:25:29 -0800

    Release v0.2.1

--- php_psr.h
+++ php_psr.h
@@ -11,8 +11,8 @@
 #endif
 
 #define PHP_PSR_NAME "psr"
-#define PHP_PSR_VERSION "0.2.0"
-#define PHP_PSR_RELEASE "2015-12-09"
+#define PHP_PSR_VERSION "0.2.1"
+#define PHP_PSR_RELEASE "2015-12-15"
 #define PHP_PSR_AUTHORS "John Boehr <jbboehr@gmail.com> (lead)"
 
 #define PHP_PSR_ABSTRACT_ME(c, f) PHP_ABSTRACT_ME(c, f, arginfo_ ## c ## _ ## f)
