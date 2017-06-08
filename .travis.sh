6a31083024a3297e564482c8c0bdc6e69a5da79b
John Boehr <jbboehr@gmail.com>
2017-06-07 21:09:08 -0700

    Drop PHP 5.4 and 5.5 support

--- .travis.sh
+++ .travis.sh
@@ -15,42 +15,26 @@ monolog_test)
 	php -d extension=modules/psr.so ./monolog/vendor/bin/phpunit -c monolog/
 	;;
 stash_init)
-	if [ "$TRAVIS_PHP_VERSION" != "5.3" ]; then
-		rm -rf Stash
-		git clone -b v1.0.0-dev https://github.com/tedious/Stash.git
-		cd Stash
-		composer install
-		rm -f vendor/psr/cache/src/*.php
-		cd ..
-	else
-		echo Stash does not support PHP 5.3
-	fi
+	rm -rf Stash
+	git clone -b v1.0.0-dev https://github.com/tedious/Stash.git
+	cd Stash
+	composer install
+	rm -f vendor/psr/cache/src/*.php
+	cd ..
 	;;
 stash_test)
-	if [ "$TRAVIS_PHP_VERSION" != "5.3" ]; then
-		php -d extension=modules/psr.so ./Stash/vendor/bin/phpunit -c Stash/
-	else
-		echo Stash does not support PHP 5.3
-	fi
+	php -d extension=modules/psr.so ./Stash/vendor/bin/phpunit -c Stash/
 	;;
 psr7_init)
-	if [ "$TRAVIS_PHP_VERSION" != "5.3" ]; then
-		rm -rf psr7
-		git clone -b $GUZZLE_PSR7_VERSION https://github.com/guzzle/psr7.git
-		cd psr7
-		composer install
-		rm -f vendor/psr/http-message/src/*.php
-		cd ..
-	else
-		echo PSR7 does not support PHP 5.3
-	fi
+	rm -rf psr7
+	git clone -b $GUZZLE_PSR7_VERSION https://github.com/guzzle/psr7.git
+	cd psr7
+	composer install
+	rm -f vendor/psr/http-message/src/*.php
+	cd ..
 	;;
 psr7_test)
-	if [ "$TRAVIS_PHP_VERSION" != "5.3" ]; then
-		php -d extension=modules/psr.so ./psr7/vendor/bin/phpunit -c psr7/
-	else
-		echo PSR7 does not support PHP 5.3
-	fi
+	php -d extension=modules/psr.so ./psr7/vendor/bin/phpunit -c psr7/
 	;;
 esac
 
