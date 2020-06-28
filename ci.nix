b77b321fbc9db6967fbd94af55956bb780a694b6
John Boehr <john@johnboehr.is>
2020-06-27 20:37:26 -0700

    CI improvements
    * Remove Nix from TravisCI
    * Test against clang in HerculesCI
    * Clean source in default.nix instead of ci.nix
    * Hack/fix for clang and asm goto
    * Test against arm64, ppc64le, and s390x in TravisCI
    * ppc64le is allow_failures due to https://bugs.php.net/bug.php?id=79744

--- ci.nix
+++ ci.nix
@@ -1,66 +1,55 @@
 let
-    generatePsrDrv = { pkgs, php }:
-        pkgs.runCommand "pecl-psr.tgz" {
-            buildInputs = [ php ];
-            src = builtins.filterSource
-                (path: type: baseNameOf path != ".idea" && baseNameOf path != ".git" && baseNameOf path != "ci.nix")
-                ./.;
-        } ''
-            cp -r $src/* .
-            pecl package | tee tmp.txt
-            pecl_tgz=$(cat tmp.txt | grep -v Warning | awk '{print $2}')
-            echo $pecl_tgz
-            cp $pecl_tgz $out
-        '';
-
-    generateTestsForPlatform = { pkgs, path, phpAttr, dist }:
+    generateTestsForPlatform = { pkgs, path, phpAttr }:
         pkgs.recurseIntoAttrs {
-            psr = let
+            gcc = let
                 php = pkgs.${phpAttr};
             in pkgs.callPackage ./default.nix {
                 inherit php;
-                phpPsrSrc = dist;
                 buildPecl = pkgs.callPackage "${path}/pkgs/build-support/build-pecl.nix" { inherit php; };
             };
-            psr-32bit = let
+
+            gcc-i686 = let
                 php = pkgs.pkgsi686Linux.${phpAttr};
             in pkgs.pkgsi686Linux.callPackage ./default.nix {
                 inherit php;
-                phpPsrSrc = dist;
                 buildPecl = pkgs.callPackage "${path}/pkgs/build-support/build-pecl.nix" { inherit php; };
+            };
+
+            clang = let
+                php = pkgs.${phpAttr};
+                stdenv = pkgs.clangStdenv;
+            in pkgs.callPackage ./default.nix {
+                inherit php stdenv;
+                buildPecl = pkgs.callPackage "${path}/pkgs/build-support/build-pecl.nix" { inherit php stdenv; };
             };
         };
 in
 builtins.mapAttrs (k: _v:
   let
     path = builtins.fetchTarball {
-        url = https://github.com/NixOS/nixpkgs/archive/release-20.03.tar.gz;
+        url = https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.03.tar.gz;
         name = "nixpkgs-20.03";
     };
     pkgs = import (path) { system = k; };
 
-    dist = generatePsrDrv {
-        inherit pkgs;
-        inherit (pkgs) php;
-    };
   in
   pkgs.recurseIntoAttrs {
     php72 = generateTestsForPlatform {
-        inherit pkgs path dist;
+        inherit pkgs path;
         phpAttr = "php72";
     };
 
     php73 = let
         php = pkgs.php73;
     in generateTestsForPlatform {
-        inherit pkgs path dist;
+        inherit pkgs path;
         phpAttr = "php73";
     };
 
     php74 = let
         php = pkgs.php74;
     in generateTestsForPlatform {
-        inherit pkgs path dist;
+        inherit pkgs path;
         phpAttr = "php74";
     };
   }
--- default.nix
+++ default.nix
@@ -1,15 +1,29 @@
+# to use a specific version of php, run: nix-shell --arg php '(import <nixpkgs> {}).php73'`
+# or clang: nix-shell --arg stdenv '(import <nixpkgs> {}).clangStdenv'
 {
   pkgs ? import <nixpkgs> {},
+  stdenv ? pkgs.stdenv,
   php ? pkgs.php,
   buildPecl ? pkgs.callPackage <nixpkgs/pkgs/build-support/build-pecl.nix> {
-    inherit php;
+    inherit php stdenv;
   },
 
+  gitignoreSource ? (import (pkgs.fetchFromGitHub {
+      owner = "hercules-ci";
+      repo = "gitignore";
+      rev = "00b237fb1813c48e20ee2021deb6f3f03843e9e4";
+      sha256 = "sha256:186pvp1y5fid8mm8c7ycjzwzhv7i6s3hh33rbi05ggrs7r3as3yy";
+  }) { inherit (pkgs) lib; }).gitignoreSource,
+
   phpPsrVersion ? null,
-  phpPsrSrc ? ./.,
-  phpPsrSha256 ? null
+  phpPsrSha256 ? null,
+  phpPsrSrc ? pkgs.lib.cleanSourceWith {
+    filter = (path: type: (builtins.all (x: x != baseNameOf path) [".idea" ".git" "ci.nix" ".travis.sh" ".travis.yml" ".ci"]));
+    src = gitignoreSource ./.;
+  }
 }:
 
 pkgs.callPackage ./derivation.nix {
-  inherit php phpPsrVersion phpPsrSrc phpPsrSha256 buildPecl;
+  inherit stdenv php buildPecl;
+  inherit phpPsrVersion phpPsrSrc phpPsrSha256;
 }
